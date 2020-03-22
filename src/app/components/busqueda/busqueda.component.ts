70e14d41e96e5bcfc3d1227f3b8b8a20be5fd06e
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-22 17:16:01 -0300

    [add] convenient go back button on all views

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,35 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PeliculasService } from 'src/app/services/peliculas.service';
+import { Location } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { PeliculasService } from "src/app/services/peliculas.service";
 
 @Component({
-  selector: 'app-busqueda',
-  templateUrl: './busqueda.component.html',
+  selector: "app-busqueda",
+  templateUrl: "./busqueda.component.html",
   styles: []
 })
 export class BusquedaComponent implements OnInit {
-  loading:Boolean;
-  terminoBusqueda:string='';
-  buscar(){
-
-    if(this.terminoBusqueda.length == 0) return null;
-    this.loading=true;
-      this._peliculasService.getSearch(this.terminoBusqueda)
-      .subscribe( res => this.loading=false)
+  loading: Boolean;
+  terminoBusqueda: string = "";
+  buscar() {
+    if (this.terminoBusqueda.length == 0) return null;
+    this.loading = true;
+    this._peliculasService
+      .getSearch(this.terminoBusqueda)
+      .subscribe(res => (this.loading = false));
   }
-  constructor(private activateRoute: ActivatedRoute, public _peliculasService: PeliculasService) {
-    this.activateRoute.params
-    .subscribe( params => {
-      if(params['termino']){
-        this.terminoBusqueda=params['termino'];
+
+  goback() {
+    this.location.back();
+  }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    public _peliculasService: PeliculasService,
+    private location: Location
+  ) {
+    this.activateRoute.params.subscribe(params => {
+      if (params["termino"]) {
+        this.terminoBusqueda = params["termino"];
         this.buscar();
       }
-    })
-   }
-
-  ngOnInit() {
-
-
+    });
   }
 
+  ngOnInit() {}
 }
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,29 +1,38 @@
-import { PeliculasService } from 'src/app/services/peliculas.service';
-import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { PeliculasService } from "src/app/services/peliculas.service";
+import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  selector: "app-category",
+  templateUrl: "./category.component.html",
+  styleUrls: ["./category.component.css"]
 })
 export class CategoryComponent implements OnInit {
-  categoria:string;
-  avaiableCategories:any[]=[];
-  peliculas:any[]=[]
-  constructor(private route:ActivatedRoute, private _peliculasService: PeliculasService) { }
+  categoria: string;
+  avaiableCategories: any[] = [];
+  peliculas: any[] = [];
+
+  goback() {
+    this.location.back();
+  }
+  constructor(
+    private route: ActivatedRoute,
+    private _peliculasService: PeliculasService,
+    private location: Location
+  ) {}
 
   ngOnInit() {
-    this._peliculasService.getavaiblegenres().subscribe( res =>{
-      this.avaiableCategories = res['genres']
-    })
-    this.route.params.subscribe( paramas => {
+    this._peliculasService.getavaiblegenres().subscribe(res => {
+      this.avaiableCategories = res["genres"];
+    });
+    this.route.params.subscribe(paramas => {
       this.categoria = paramas.categoria;
-      this._peliculasService.getCategory(this.categoria).subscribe( res => {
-        this.peliculas = res['results'].filter( item => item !== null && item.poster_path)
-      })
-
-    })
+      this._peliculasService.getCategory(this.categoria).subscribe(res => {
+        this.peliculas = res["results"].filter(
+          item => item !== null && item.poster_path
+        );
+      });
+    });
   }
-
 }
