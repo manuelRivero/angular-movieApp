d2825635542335e48f8d50af07896435c10c2846
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-22 16:21:35 -0300

    [add] categories dropdown in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,23 @@
-import { Component} from '@angular/core';
+import { PeliculasService } from 'src/app/services/peliculas.service';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styles: []
 })
-export class HeaderComponent {
-  busqueda:string=""
-  constructor(private router:Router) { }
+export class HeaderComponent implements OnInit{
+  busqueda:string="";
+  categorias$:any[];
+  constructor(private router:Router, private _peliculasService: PeliculasService) { }
   buscar(){
     this.router.navigate(['/busqueda', this.busqueda]);
   }
-
+ngOnInit(){
+  this._peliculasService.getavaiblegenres().subscribe( (res)=>{
+    this.categorias$=res['genres'];
+  })
+}
 }
