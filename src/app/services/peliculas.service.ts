08045d2769877af8b1d281450f1c90559a212039
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-22 16:24:12 -0300

    [add] new request for categories and new features

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,67 +1,92 @@
-import { map } from 'rxjs/operators';
-import { HttpClient, JsonpClientBackend, JsonpInterceptor } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { map } from "rxjs/operators";
+import {
+  HttpClient,
+  JsonpClientBackend,
+  JsonpInterceptor
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
 
 const apiKey = `160914de780d5e63e887a59b31812e69`;
 const apiUrl = `https://api.themoviedb.org/3`;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-
 export class PeliculasService {
+  constructor(private http: HttpClient) {}
 
-  constructor( private http:HttpClient) { }
+  resultadosBusqueda: any;
 
-  resultadosBusqueda:any;
-
-
-  getPopulars(){
-    let url=`${apiUrl}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
-    return this.http.jsonp(url, 'JSONP_CALLBACK')
+  getPopulars() {
+    let url = `${apiUrl}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
+    return this.http.jsonp(url, "JSONP_CALLBACK");
   }
 
-  getKidsPopulars(){
-    let url=`${apiUrl}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
-    return this.http.jsonp(url, 'JSONP_CALLBACK')
-  }
-
-  getSearch(terminoBusqueda:string){
+  getSearch(terminoBusqueda: string) {
     let url = `${apiUrl}/search/movie?query=${terminoBusqueda}&sort_by=popularity.desc&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
-    return this.http.jsonp(url, 'JSONP_CALLBACK').pipe(
-      map( res => {
-        this.resultadosBusqueda= res['results'];
-        return res})
-    )
+    return this.http.jsonp(url, "JSONP_CALLBACK").pipe(
+      map(res => {
+        this.resultadosBusqueda = res["results"];
+        return res;
+      })
+    );
   }
 
-  getReleases(){
+  getReleases() {
     let desde = new Date();
     let hasta = new Date();
-    hasta.setDate( hasta.getDate()+7 );
+    hasta.setDate(hasta.getDate() + 7);
 
-    let desdeStr = `${desde.getFullYear()}-${desde.getMonth()+1 < 10 ? '0' :''}${desde.getMonth()+1}-${desde.getDate() < 10 ? '0' : ''}${desde.getDate()}`
-    let hastaStr = `${hasta.getFullYear()}-${hasta.getMonth()+1 < 10 ? '0' :''}${hasta.getMonth()+1}-${hasta.getDate() < 10 ? '0' : ''}${hasta.getDate()}`
+    let desdeStr = `${desde.getFullYear()}-${
+      desde.getMonth() + 1 < 10 ? "0" : ""
+    }${desde.getMonth() + 1}-${
+      desde.getDate() < 10 ? "0" : ""
+    }${desde.getDate()}`;
+    let hastaStr = `${hasta.getFullYear()}-${
+      hasta.getMonth() + 1 < 10 ? "0" : ""
+    }${hasta.getMonth() + 1}-${
+      hasta.getDate() < 10 ? "0" : ""
+    }${hasta.getDate()}`;
 
-
-    let url = `${apiUrl}/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
-    console.log(url)
-    return this.http.jsonp( url, 'JSONP_CALLBACK' )
+    let url = `${apiUrl}/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&sort_by=popularity.desc&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
+    console.log(url);
+    return this.http.jsonp(url, "JSONP_CALLBACK");
   }
 
-  getMovie(movieId:string){
-    let url= `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
-    return this.http.jsonp(url, 'JSONP_CALLBACK')
+  getavaiblegenres() {
+    let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
+    return this.http.jsonp(url, "JSONP_CALLBACK").pipe(
+      map(res => {
+        return res;
+      })
+    );
   }
 
-  getSimilar(gendersId:[]){
-    let gender= gendersId.map( gender => gender['id']).join();
-    let url =`https://api.themoviedb.org/3/discover/movie?with_genres=${gender}&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`
+  getCategory(category:string):Observable<any>{
+    let url = `${apiUrl}/discover/movie?with_genres=${category}&primary_release_year=2019&sort_by=release_date.desc?&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`
     console.log(url)
-    return this.http.jsonp(url, 'JSONP_CALLBACK')
+    return this.http.jsonp(url, "JSONP_CALLBACK").pipe(
+      map(res => {
+        return res;
+      })
+    );
+  }
+
+
+  getMovie(movieId: string) {
+    let url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
+    console.log(url)
+    return this.http.jsonp(url, "JSONP_CALLBACK");
+  }
+
+  getSimilar(gendersId: []) {
+    let gender = gendersId.map(gender => gender["id"]).join();
+    let url = `https://api.themoviedb.org/3/discover/movie?with_genres=${gender}&api_key=${apiKey}&language=es&callback=JSONP_CALLBACK`;
+    console.log(url);
+    return this.http.jsonp(url, "JSONP_CALLBACK");
   }
 }
-
 
 //https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-09-02&primary_release_date.lte=2019-09-09&api_key=160914de780d5e63e887a59b31812e69&language=es&callback=JSONP_CALLBACK
 //https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-9-2&primary_release_date.lte=2019-9-9&api_key=160914de780d5e63e887a59b31812e69&language=es&callback=JSONP_CALLBACK
